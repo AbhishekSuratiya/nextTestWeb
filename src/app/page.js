1e7b0464eeb2c8206e3e14b32345c3971875a58f
Abhishek Suratiya <abhishek.suratiya@planify.in>
2024-11-04 22:53:53 +0530

    2

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,26 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p className="text-orange-200 text-3xl">
-          This is Next App
-        </p>
+    return (
+        <div
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <p className="text-orange-200 text-3xl">
+                    This is Next App
+                </p>
 
-        <Link href={'/about'}>
-          <button className="text-2xl">Go to About</button>
-        </Link>
-      </main>
-    </div>
-  );
+                <Link href={'/about'}>
+                    <button className="text-2xl">Go to About</button>
+                </Link>
+                <button className="text-2xl" onClick={() => {
+                    window.location.href = 'planify://tradebook';
+                }}>Trade book
+                </button>
+                <button className="text-2xl" onClick={() => {
+                    window.location.href = 'planify://';
+                }}>Open App
+                </button>
+            </main>
+        </div>
+    );
 }
