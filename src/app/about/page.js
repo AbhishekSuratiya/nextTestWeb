863c77ec24a9d973151c357c5cc1729feb9c1fd1
Abhishek Suratiya <abhishek.suratiya@planify.in>
2024-11-04 14:20:02 +0530

    1

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,3 @@
-'use client'
-
 import Link from "next/link";
 
 export default function Home() {
@@ -8,11 +6,10 @@ export default function Home() {
       className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <p className="text-orange-200 text-3xl">
-          This is Next App
+          {`This is Next App's About Page`}
         </p>
-
-        <Link href={'/about'}>
-          <button className="text-2xl">Go to About</button>
+        <Link href={'/'}>
+          <button className="text-2xl">Go to Home</button>
         </Link>
       </main>
     </div>
